a37e8b8250c5e00de9886399b9328584c9ddfcd1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-05-08 18:02:10 -0700

    Simple commits

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,78 @@
+use std::collections::HashSet;
+
 use iced::Center;
 use iced::widget::{Column, button, column, text};
 
 pub fn main() -> iced::Result {
-    iced::run("A cool counter", Counter::update, Counter::view)
+    iced::run("Sync Demo", Counter::update, Counter::view)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct CommitId {
+    user_id: u64,
+    counter: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Commit {
+    id: CommitId,
+    change: i64,
 }
 
 #[derive(Default)]
 struct Counter {
-    value: i64,
+    user_id: u64,
+    commits: HashSet<Commit>,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    Increment,
-    Decrement,
+    Change(i64),
+    Import,
 }
 
 impl Counter {
     fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                self.value += 1;
+            Message::Change(change) => {
+                self.commits.insert(Commit {
+                    id: CommitId {
+                        user_id: self.user_id,
+                        counter: self.commits.len(),
+                    },
+                    change,
+                });
             }
-            Message::Decrement => {
-                self.value -= 1;
+            Message::Import => {
+                self.commits.insert(Commit {
+                    id: CommitId {
+                        user_id: 1,
+                        counter: 0,
+                    },
+                    change: 1,
+                });
+                self.commits.insert(Commit {
+                    id: CommitId {
+                        user_id: 1,
+                        counter: 1,
+                    },
+                    change: 1,
+                });
             }
         }
     }
 
     fn view(&self) -> Column<Message> {
+        // println!("Commits: {:#?}", self.commits);
+        let number = self
+            .commits
+            .iter()
+            .fold(0, |acc, commit| acc + commit.change);
         column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::Decrement)
+            button("Increment").on_press(Message::Change(1)),
+            text(number).size(50),
+            button("Decrement").on_press(Message::Change(-1)),
+            button("Import").on_press(Message::Import)
         ]
         .padding(20)
         .align_x(Center)
